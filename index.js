4e28ab4f7eb69070fa3ac152fcaa7498c764c17b
Kevin Leguizamon <kevin_Dev27@outlook.com>
2024-04-29 05:03:29 -0300

    se moficaron archivos main.js, data.json, package.json

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const server = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const routerProductos = require("./back/src/routes/index.js");
+
+const PORT = process.env.PORT || 3000;
+
+//Rutas que vamos a tener en la app
+server.get("/", (req, res) => {
+  res.send("API TIENDA");
+});
+
+server.use("/api", routerProductos);
+
+//escucha el puerto 3000
+server.listen(PORT, () => {
+  console.log(`server running in http://localhost:${PORT}`);
+});
